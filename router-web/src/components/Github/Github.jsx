de925bc8f0957a4c15d29c5f009fcf8aa3440e46
sahilTit <titarmaresahil2003@gmail.com>
2024-02-16 12:18:56 +0530

    16-02-2024

---
+++ router-web/src/components/Github/Github.jsx
@@ -0,0 +1,30 @@
+// import { useState } from "react";
+// import { useEffect } from "react";
+
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+    const data= useLoaderData()
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch("https://api.github.com/users/sahilTit")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+//         setData(data);
+//       });
+//   }, []);
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github name:{data.name}
+      <img src={data.avatar_url} alt="" style={{ width: "10rem" }} />
+    </div>
+  );
+}
+
+export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/sahilTit");
+  return response.json();
+};

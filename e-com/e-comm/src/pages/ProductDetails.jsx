6fbb2040f11c15f931e1db586992d4846c9c765a
sahilTit <titarmaresahil2003@gmail.com>
2024-03-13 19:09:19 +0530

    13-03-2024

--- e-com/e-comm/src/pages/ProductDetails.jsx
+++ e-com/e-comm/src/pages/ProductDetails.jsx
@@ -1,5 +1,47 @@
+import { useParams } from "react-router-dom";
+import Helmet from "../components/helmete/Helmete";
+import CommonSection from "../components/Ui/CommonSection";
+import products from "../assets/data/products";
+import { IoStar } from "react-icons/io5";
+import { IoMdStarHalf } from "react-icons/io";
+
+import "../styles/ProductsDetails.css";
 function ProductDetails() {
-  return <div>ProductDetails</div>;
+  const { id } = useParams();
+  const product = products.find((item) => item.id === id);
+  const {
+    productName,
+    imgUrl,
+    price,
+    description,
+    reviews,
+    shortDesc,
+    avgRating,
+  } = product;
+  return (
+    <Helmet>
+      {/* <CommonSection></CommonSection> */}
+      <section className="prodetail-sec">
+        <div>
+          <img src={imgUrl} alt="" className="prodetail-image" />
+        </div>
+        <div>
+          <h1>{productName}</h1>
+          <span>
+            <IoStar />
+            <IoStar />
+            <IoStar />
+            <IoStar />
+            <IoMdStarHalf />
+          </span>
+          <p>{avgRating}(avgRating)</p>
+          <p>{price}</p>
+          <p>{shortDesc}</p>
+          <button className="adtcrt">AddToCart</button>
+        </div>
+      </section>
+    </Helmet>
+  );
 }
 
 export default ProductDetails;
--- e-com/e-comm/src/pages/Shop.jsx
+++ e-com/e-comm/src/pages/Shop.jsx
@@ -3,19 +3,41 @@ import { useState } from "react";
 import CommonSection from "../components/Ui/CommonSection";
 import products from "../assets/data/products";
 import { CiSearch } from "react-icons/ci";
-import productList from "../components/Ui/ProductsList";
+import ProductList from "../components/Ui/ProductsList";
 
 function Shop() {
   const [productsData, setProductsData] = useState(products);
 
   const handleFilter = (e) => {
     const filterValue = e.target.value;
-    if (filteredValue === "sofa") {
+    if (filterValue === "sofa") {
       const filteredProduct = products.filter(
         (item) => item.category === "sofa"
       );
       setProductsData(filteredProduct);
+    } else if (filterValue === "mobile") {
+      const filteredProduct = products.filter(
+        (item) => item.category === "mobile"
+      );
+      setProductsData(filteredProduct);
+    } else if (filterValue === "watch") {
+      const filteredProduct = products.filter(
+        (item) => item.category === "watch"
+      );
+      setProductsData(filteredProduct);
+    } else if (filterValue === "wireless") {
+      const filteredProduct = products.filter(
+        (item) => item.category === "wireless"
+      );
+      setProductsData(filteredProduct);
     }
+  };
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+    const searchProduct = products.filter((item) =>
+      item.productName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setProductsData(searchProduct);
   };
   return (
     <Helmete title={"Shop"}>
@@ -26,7 +48,7 @@ function Shop() {
             <option>Filter By Category</option>
             <option value="sofa">sofa</option>
             <option value="mobile">mobile</option>
-            <option value="chai">chai</option>
+            {/* <option value="chai">chai</option> */}
             <option value="watch">watch</option>
             <option value="wireless">wireless</option>
           </select>
@@ -39,17 +61,17 @@ function Shop() {
           </select>
         </div>
         <div className="serachbar">
-          <input type="text" placeholder="search..." />
+          <input type="text" placeholder="search..." onChange={handleSearch} />
           <span>
             <CiSearch />
           </span>
         </div>
       </section>
-      <section>
+      <section className="products-filter">
         {productsData.length === 0 ? (
-          <h1>No productfound</h1>
+          <h1 className="not-found">No productfound!</h1>
         ) : (
-          <productList data={productList} />
+          <ProductList data={productsData} />
         )}
       </section>
     </Helmete>
